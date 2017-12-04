ed4d83c2b1f03dd994ac0bd3361ff4963f2cdfce
Dado1513 <dave.caputo93@gmail.com>
2017-12-04 11:54:02 +0100

    add insert value main

--- Main.py
+++ Main.py
@@ -2,8 +2,27 @@ import Dataframe
 import AnonymizationCAHD
 
 if __name__ == "__main__":
-    df =Dataframe.Dataframe('online_retail_transaction.csv')
-    df.compute_band_matrix(dim_finale=50,nome_file_item="lista_items.txt",num_sensibile=4)
-    cahd = AnonymizationCAHD.AnonymizationCAHD(df)
+
+    dim_finale = eval(input("Dimensione del dataset: "))
+    num_sensibile = eval(input("Numero di item sensibili da testare: "))
+    grado_privacy = eval(input("Grado di privacy desiderato: "))
+    alpha = eval(input(
+        "Inserire valore di alpha (p*alfa check, valore ottimale = 3): "))
+    print("")
+    print("Read Dataset")
+    df = Dataframe.Dataframe('online_retail_transaction.csv')
+    print("")
+    print("Calcolo la band matrix")
+    df.compute_band_matrix(
+        dim_finale=dim_finale,
+        nome_file_item="lista_items.txt",
+        num_sensibile=num_sensibile)
+    print("")
+    print("Eseguo Anonimizzazione")
+    cahd = AnonymizationCAHD.AnonymizationCAHD(
+        df,
+        grado_privacy=grado_privacy,
+        alfa=alpha)
+    print("Anonimizzazione eseguita")
     cahd.CAHD_algorithm()
-    print(cahd.sd_gruppi)
+    #print(cahd.sd_gruppi)
