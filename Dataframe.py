88877f86b84518b1008074095ae80700c1b314c9
Dado1513 <dave.caputo93@gmail.com>
2017-12-06 20:49:51 +0100

    add part if dataset isn't square

--- Dataframe.py
+++ Dataframe.py
@@ -27,55 +27,118 @@ class Dataframe:
             estrapola a caso item_sensibili
         """
         original_dataset = self.dataframe
-        if original_dataset is not None and len(original_dataset) >= dim_finale and len(original_dataset.columns) >= dim_finale:
+        if original_dataset is not None and nome_file_item is not None and len(original_dataset) >= dim_finale and len(original_dataset.columns) >= dim_finale:
 
             # leggo nomi items
-            file = open(nome_file_item, "r")
-            items = file.read().splitlines()
-
+            file_read = open(nome_file_item, "r")
+            items = file_read.read().splitlines()
+            file_read.close()
             # permuto righe e colonne del df inizale e prendo le prime :dim_finale
             np.random.seed(seed=13)
             random_column = np.random.permutation(original_dataset.shape[1])[:dim_finale]
             random_row = np.random.permutation(original_dataset.shape[0])[:dim_finale]
             # recupero gli item selezionati nel relativo ordine == colonne
             items_reordered = [items[i] for i in random_column]
-
             # df selezionato e square
             # eliminare le righe nulle a priori
             df_square = original_dataset.iloc[random_row][random_column];
             # eliminare solo se items sensibili nulli
-
             # selezioni gli utlimi num_sensibili come item sensibili
             # check se esiste almeno un item sensibile
-
-            # remove dati sensibili add zero --> compute RCM --> remove zero e add dati sensibili
-            # df_sensibili = df_square[df_square.columns[-num_sensibile:]]
-            # df_common_items = df_square[df_square.columns[0:-num_sensibile]]
-            # zero_data_to_add = np.zeros(shape=(len(df_common_items),num_sensibile))
-            # columns_to_add = ["temp_"+str(x) for x in range(0,num_sensibile)]
-            # df_zeri = pd.DataFrame(zero_data_to_add, columns=columns_to_add,index=df_common_items.index,dtype='uint8')
-            # df_square = pd.concat([df_common_items,df_zeri],axis=1)
-            # print(df_zeri.shape) dim_finalexnum_sensibili
-            # print(df_square.shape)
-            # subplot with y condiviso
-
             f, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
-
             # check che le colonne sensibili non siano nulle
             lista_sensibili = df_square.columns[-num_sensibile:]
             # plot matrice sparsa iniziale
             # plt
             ax1.spy(df_square, marker='.', markersize='1')
             #ax1.show()
-
             # applicazione algoritmo RCM
             sparse = csr_matrix(df_square)
             order = reverse_cuthill_mckee(sparse)
-
             # solo se add gli 0
             # riordino i dati sensibili
             # df_sensibili = df_sensibili.iloc[order]
+            # ora devo prendere gli item selzionati prima e riordinarli ancora
+            # secondo quello scritto in order quindi
+            items_final = [items_reordered[i] for i in order]
+            column_reordered = [df_square.columns[i] for i in order]
+            # creo gl item indicizzati con la colonna
+            items_final = dict(zip(column_reordered,items_final))
+            # df bandizzato
+            df_square_band = df_square.iloc[order][column_reordered]
+            # plotto
+            ax2.spy(df_square_band, marker='.', markersize='1')
+            #ax2.show()
+            if plot:
+                plt.show()
+            # banda dataframe inizale
+            [i, j] = np.where(df_square == 1)
+            bw = max(i-j) + 1
+            self.original_band = bw
+            print("Bandwidth first RCM", bw)
 
+            # banda dataframe dopo RCM
+            [i, j] = np.where(df_square_band == 1)
+            bw = max(i-j) + 1
+            self.band_after_rcm = bw
+            print("Bandwidth after RCM", bw)
+
+            self.dataframe_bandizzato = df_square_band
+            self.items_final = items_final
+            self.lista_sensibili = lista_sensibili
+
+        elif original_dataset is not None and nome_file_item is not None and len(original_dataset) >= dim_finale:
+            # devo essere sicuro di avere il numero di righe giusto
+            # allora devo squadrarlo con la dimensione finale definita dall'utente
+            np.random.seed(seed=30)
+            random_row = np.random.permutation(original_dataset.shape[0])
+            original_dataset = original_dataset.iloc[random_row][:dim_finale]
+            file_read = open(nome_file_item, "r")
+            items = file_read.read().splitlines()
+            file_read.close()
+            original_dataset = original_dataset.reset_index()
+            original_dataset.drop('index',axis=1,inplace=True)
+            columns = original_dataset.columns
+            zero_data_to_add = np.zeros(shape=(len(original_dataset),len(original_dataset)-len(columns)))
+            # aggiungo li zero con la dimensione finale relativa
+            #zero_data_to_add.shape
+            columns_to_add = ["temp"+str(x) for x in range(0,len(original_dataset)-len(columns))]
+            #columns_to_add
+            df_to_add = pd.DataFrame(zero_data_to_add, columns=columns_to_add, index=original_dataset.index,dtype='uint8')
+            #df_to_add
+            # creo il dataset completo aggiungendo tutti gli zeri che mancano
+            original_dataset = pd.concat([original_dataset, df_to_add], axis=1)
+            # permuto righe e colonne del df inizale e prendo le prime :dim_finale
+            np.random.seed(seed=13)
+            items = original_dataset.columns
+
+            random_column = np.random.permutation(original_dataset.shape[1])
+            random_row = np.random.permutation(original_dataset.shape[0])
+
+            original_dataset.columns = [i for i in range(0,len(original_dataset.columns))]
+            #print(original_dataset.columns)
+            # recupero gli item selezionati nel relativo ordine == colonne
+            items_reordered = [items[i] for i in random_column]
+            # df selezionato e square
+            # eliminare le righe nulle a priori
+
+            df_square = original_dataset.iloc[random_row][random_column];
+            # eliminare solo se items sensibili nulli
+            # selezioni gli utlimi num_sensibili come item sensibili
+            # check se esiste almeno un item sensibile
+            f, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
+            # check che le colonne sensibili non siano nulle
+            lista_sensibili = df_square.columns[-num_sensibile:]
+            # plot matrice sparsa iniziale
+            # plt
+            ax1.spy(df_square, marker='.', markersize='1')
+            #ax1.show()
+            # applicazione algoritmo RCM
+            sparse = csr_matrix(df_square)
+            order = reverse_cuthill_mckee(sparse)
+            # solo se add gli 0
+            # riordino i dati sensibili
+            # df_sensibili = df_sensibili.iloc[order]
             # ora devo prendere gli item selzionati prima e riordinarli ancora
             # secondo quello scritto in order quindi
             items_final = [items_reordered[i] for i in order]
--- Main.py
+++ Main.py
@@ -1,22 +1,26 @@
 import Dataframe
 import AnonymizationCAHD
 if __name__ == "__main__":
-    
+
     dim_finale = eval(input("Dimensione del dataset: "))
     num_sensibile = eval(input("Numero di item sensibili da testare: "))
     grado_privacy = eval(input("Grado di privacy desiderato: "))
     alpha = eval(input(
         "Inserire valore di alpha (p*alfa check, valore ottimale = 3): "))
-
+    nameFile = eval(input("Insert name/path file: "))
+    listaItem  = eval(input("Inserire il nome del file contenete gli items: "))
+    # testing
+    # nameFile = "Dataset Paper/dataBMS1_transiction.csv"
+    # listItem = "Dataset Paper/lista_items_BMS1.txt"
     print("")
     print("Read Dataset")
-    df = Dataframe.Dataframe('Data/online_retail_transaction.csv')
+    df = Dataframex.Dataframe(nameFile)
 
     print("")
     print("Calcolo la band matrix")
     df.compute_band_matrix(
         dim_finale=dim_finale,
-        nome_file_item="Data/lista_items.txt",
+        nome_file_item=listItem,
         num_sensibile=num_sensibile)
 
     print("")
