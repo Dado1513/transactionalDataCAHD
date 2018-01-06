fix compute KL

Main_KL_Divergence.py
@@ -4,7 +4,7 @@ import time
 import numpy as np
 import KLDivergence
 import random
-import matplotlib.pyplot as plt
+import operator
 
 if __name__ == "__main__":
 
@@ -14,7 +14,6 @@ if __name__ == "__main__":
     num_sensibile = 10
     #grado_privacy = eval(input("Grado di privacy desiderato: "))
     privacy_list = [4, 6, 8, 10, 12, 14, 16, 18, 20]
-    privacy_list = [4]
 
     KLs = list()
     for grado_privacy in privacy_list:
@@ -24,10 +23,10 @@ if __name__ == "__main__":
         #nameFile = eval(input("Insert name/path file: "))
         #listaItem = eval(input("Inserire il nome del file contenete gli items: "))
         # testing
-        #nameFile = "Dataset Paper/dataBMS1_transiction.csv"
-        #listaItem = "Dataset Paper/lista_items_BMS1.txt"
-        nameFile = "Dataset Paper/dataBMS2_transiction.csv"
-        listaItem = "Dataset Paper/lista_items_BMS2.txt"
+        nameFile = "Dataset Paper/dataBMS1_transiction.csv"
+        listaItem = "Dataset Paper/lista_items_BMS1.txt"
+        #nameFile = "Dataset Paper/dataBMS2_transiction.csv"
+        #listaItem = "Dataset Paper/lista_items_BMS2.txt"
         print("")
         print("Read Dataset")
         df = Dataframe.Dataframe(nameFile)
@@ -45,6 +44,8 @@ if __name__ == "__main__":
             grado_privacy=grado_privacy,
             alfa=alpha)
 
+        cahd.compute_hist()
+        hist_item = cahd.hist
         print("Eseguo Anonimizzazione")
         cahd.CAHD_algorithm()
         end_time = time.time() - start_time
@@ -59,8 +60,6 @@ if __name__ == "__main__":
         # p fissato vedi sopra con p e m variabile
 
         # per saper i dati sensibili
-        print(cahd.hist)
-        print("")
         r = 4 # numero di QID nella query
         all_item = list(df.items_final.keys())
         columns_item_sensibili = df.lista_sensibili.values.tolist()
@@ -76,14 +75,18 @@ if __name__ == "__main__":
             if temp not in QID_select:
                 QID_select.append(temp)
         all_value = KLDivergence.get_all_combination_of_n(r)
-        item_sensibile = random.choice(columns_item_sensibili)
+        # get max value of sensibile data
+
+        item_sensibile = int(max(hist_item.keys(), key=(lambda k: hist_item[k])))
+        print(hist_item)
+        print(item_sensibile)
         KL_Divergence = 0
         for valori in all_value:
             actsc = KLDivergence.compute_act_s_in_c(dataframe_bandizzato, QID_select, valori, item_sensibile)
-            print("acts", actsc)
+            #print("acts", actsc)
             estsc = KLDivergence.compute_est_s_in_c(dataframe_bandizzato,cahd.sd_gruppi,
                                                     cahd.lista_gruppi, QID_select, valori, item_sensibile)
-            print("est", estsc)
+            #print("est", estsc)
             if actsc > 0 and estsc > 0:
                 temp = actsc * np.log(actsc/estsc)
             else:
@@ -91,15 +94,15 @@ if __name__ == "__main__":
             #print("KL_Divergence i = ", temp )
             KL_Divergence = KL_Divergence + temp
         KLs.append(KL_Divergence)
-        print(KL_Divergence)
+        print("p: "+str(grado_privacy)+" m: "+str(num_sensibile)+" KL: "+str(KL_Divergence))
     if nameFile == "Dataset Paper/dataBMS2_transiction.csv":
         open_file = "valoriKLD_BMS2_"+str(num_sensibile)+".txt"
     else:
         open_file = "valoriKLD_BMS1_"+str(num_sensibile)+".txt"
 
     file = open(open_file,"w")
-    file.write("num_sensibili " + str(num_sensibile) + "\n" );
-    for index in range(0,len(privacy_list)):
-        file.write(str(privacy_list[index]) + " " + str(KLs[index]) + "\n" )
+    file.write("num_sensibili " + str(num_sensibile) + "\n")
+    for index in range(0, len(privacy_list)):
+        file.write(str(privacy_list[index]) + " " + str(KLs[index]) + "\n")
     file.close()
 